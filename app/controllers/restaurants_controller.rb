63970d1b618d3a27c9b71978de929b54b5755f76
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-15 16:59:01 +0100

    completed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,48 @@
 class RestaurantsController < ActionController::Base
+
+  before_action :set_restaurant, only: %i[show edit update destroy]
+
+  def index
+    @index = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+    @restaurant.save
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pizzapizza = { name: "PizzaPizza", address: "123 boulevard de France - 75841", phone_number: "02.15.48.79.23", category: "italian" }
+traveljapan = { name: "TravelJapan", address: "45 rue des Mims - 75013", phone_number: "05.01.08.98.85", category: "japanese" }
+bonnefrite = { name: "La Bonne Frite", address: "14 rue des Francs - 01475", phone_number: "02.14.25.56.75", category: "belgian" }
+royalmo = { name: "Au Royal Morangis", address: "14 boulevard de France - 91000", phone_number: "02.12.45.45.56", category: "chinese" }
+lentrecote = { name: "L'entrecote", address: "45 avenue du plessis - 91260", phone_number: "01.25.25.56.89", category: "french" }
+bolognaise = { name: "La Bolognaise", address: "place d'Italie", phone_number: "02.12.52.58.75", category: "italian" }
+cantonnais = { name: "Le Cantonnais", address: "Somewhere", phone_number: "05.14.58.58.98", category: "chinese" }
+
+[pizzapizza, traveljapan, bonnefrite, royalmo, lentrecote, bolognaise, cantonnais].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
