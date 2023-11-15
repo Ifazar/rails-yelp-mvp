48aef610760b52283155f2273273436b70ae546e
RAZAFIMANOHIHAJA Aurelien <aurelienrazafi@gmail.com>
2023-11-15 10:14:01 +0100

    add model comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_085705) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_091338) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
